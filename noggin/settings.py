c62daa53f87d6f820f737d5805e6a072f5821ef6
Lugene <chocolate365@comcast.net>
2023-02-02 19:48:40 -0500

    fixed settings file

--- noggin/settings.py
+++ noggin/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'tasks/static/tasks/'
+STATIC_ROOT = BASE_DIR / 'tasks/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media/'
 MEDIA_URL = 'media/'
